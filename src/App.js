a1fd678098dbe60df3b234b98015dfb7a8680ac3
Jerrod Mathis <mathis_jerrod@ne.bah.com>
2019-12-16 10:25:22 -0500

    created scroll component, practicing props.children

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
+import Scroll from './Scroll';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -40,7 +41,9 @@ class App extends Component {
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={ filteredRobots }/>
+                    <Scroll>
+                        <CardList robots={ filteredRobots }/>
+                    </Scroll>
                 </div>
             );
         }
