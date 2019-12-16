947e5dc6cd5c2e39b88e3e4f6cab7e36b8d2dea0
Jerrod Mathis <mathis_jerrod@ne.bah.com>
2019-12-16 11:38:35 -0500

    Updating file imports with spelling correction

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import ErrorBoundry from '../components/ErrorBoundry';
+import ErrorBoundary from '../components/ErrorBoundary';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 import './App.css';
@@ -44,9 +44,9 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <ErrorBoundry>
+                        <ErrorBoundary>
                             <CardList robots={ filteredRobots }/>
-                        </ErrorBoundry>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
